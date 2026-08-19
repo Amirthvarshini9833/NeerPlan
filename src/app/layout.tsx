a281af707f0f3d33fd1fd8b44a88bc171ab80dd4
Amirthavarshini9833 <amirthavarshini9833@gmail.com>
2026-08-19 21:57:34 +0530

    Style dashboard assessment cards

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./quote-form.css";
+import "./dashboard-style.css";
 import { SiteHeader } from "@/components/site-header";
 import { LanguageProvider } from "@/components/language-provider";
 
